5415d3734cbc8e78d8ccb195cb5c03e269133059
xsephiroth <xenos.sephiroth@gmail.com>
2024-10-20 21:36:28 +0800

    Init

---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+  "os/exec"
+	"github.com/gin-gonic/gin"
+)
+
+var page = `
+  <form method="POST" action="/poweroff">
+    <button type="submit">POWEROFF</button>
+  </form>
+`
+
+func main() {
+	g := gin.Default()
+
+	g.GET("", func(ctx *gin.Context) {
+    ctx.Header("Content-Type", "text/html")
+    ctx.String(200, page)
+	})
+
+	g.POST("poweroff", func(ctx *gin.Context) {
+		fmt.Println("got poweroff")
+    cmd := exec.Command("poweroff")
+    out, err := cmd.Output()
+    if err != nil {
+      panic(err)
+    }
+    fmt.Printf("%s\n", out)
+		ctx.String(200, "ok")
+	})
+
+
+	g.Run(":9876")
+}
